70b695817e11e26c7a0841d5b59ea818cdfcffd1
basvanderkamp <basvanderkamp@hotmail.nl>
2022-09-15 20:28:38 +0200

    begonnen met stappen plan tot stap 8

--- src/NumbersToLetters.java
+++ src/NumbersToLetters.java
@@ -1,7 +1,14 @@
+import java.lang.reflect.Array;
+
 public class NumbersToLetters {
 
     public static void main(String[] args) {
-       //hashmap maken
+
+
+        Integer[] numeric = {1,2,3,4,5,6,7,8,9,0};
+
+        String[] alphabetic = {"een","twee","drie","vier","vijf","zes","zeven","acht","negen","nul"};
+
 
        //input van de user krijgen en gebruiken
 
--- src/Translator.java
+++ src/Translator.java
@@ -1,2 +1,15 @@
+import java.util.HashMap;
+
 public class Translator {
+
+    HashMap <Integer,String> numericAlpha = new HashMap<Integer,String>();
+
+
+    public Translator(String[] alphabetic, Integer[] numeric) {
+
+        for (Integer i : numeric) {
+
+
+        }
+    }
 }
